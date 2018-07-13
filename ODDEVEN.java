3ffa5c73642cde826feec06422a6b2e75826c5b3
GOKULRAJ <41099493+GOKULGAYU@users.noreply.github.com>
2018-07-13 10:00:01 +0530

    Add files via upload

---
+++ ODDEVEN.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ODDEVEN {
+	public static void main(String[] args) {
+		Scanner gk=new  Scanner(System.in);
+		int a=gk.nextInt();
+		if(a%2==0){
+			System.out.println("Even");}else System.out.println("Odd");
+}}
