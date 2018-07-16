c6cd9beba1e3c44571f2e64560d9281cc17e7843
GOKULRAJ <41099493+GOKULGAYU@users.noreply.github.com>
2018-07-16 15:42:13 +0530

    Add files via upload

---
+++ count_digits.java
@@ -0,0 +1,10 @@
+package ASDF;
+import java.util.Scanner;
+public class count_digits {
+public static void main(String[] args) {
+	Scanner gk=new Scanner(System.in);
+	int a=gk.nextInt();
+	String s=String.valueOf(a);
+	System.out.println(s.length());
+}
+}
