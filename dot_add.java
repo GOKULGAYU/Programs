2b19ee3bfc90fbb528700e89cce19389253b6d58
GOKULRAJ <41099493+GOKULGAYU@users.noreply.github.com>
2018-07-16 17:39:26 +0530

    Add files via upload

---
+++ dot_add.java
@@ -0,0 +1,10 @@
+package ASDF;
+import java.util.Scanner;
+public class dot_add {
+	public static void main(String[] args) {
+		Scanner gk=new Scanner(System.in);
+		String s=gk.next();
+		System.out.println(s+".");
+	}
+
+}
