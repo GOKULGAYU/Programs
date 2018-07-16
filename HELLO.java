9ab94d5a7c021bea917a675f0969bb36a640e198
GOKULRAJ <41099493+GOKULGAYU@users.noreply.github.com>
2018-07-16 14:47:41 +0530

    Add files via upload

---
+++ HELLO.java
@@ -0,0 +1,12 @@
+package ASDF;
+import java.util.Scanner;
+public class HELLO {
+	public static void main(String[] args) {
+		Scanner gk=new Scanner(System.in);
+		int N=gk.nextInt();
+		for(int i=0;i<N;i++){
+			System.out.println("HELLO");
+		}
+	}
+
+}
