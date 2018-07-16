f20060fd5197e2eb985f1d10abe0c273df5cf876
GOKULRAJ <41099493+GOKULGAYU@users.noreply.github.com>
2018-07-16 16:59:16 +0530

    Add files via upload

---
+++ even_bet.java
@@ -0,0 +1,12 @@
+package ASDF;
+
+import java.util.Scanner;
+
+public class even_bet {
+	public static void main(String[] args) {
+		Scanner gk=new Scanner(System.in);
+		int a=gk.nextInt();int b=gk.nextInt();
+		for(int i=a+1;i<b;i++){
+			if(i%2==0){System.out.print(i+" ");}
+	}}
+}
