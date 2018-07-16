c0217f57fc2aca42de6aecf8c4d239c8cc67c5d2
GOKULRAJ <41099493+GOKULGAYU@users.noreply.github.com>
2018-07-16 15:52:40 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class power {
+	public static void main(String[] args) {
+		Scanner gk=new Scanner(System.in);
+		int a=gk.nextInt();
+		int b=gk.nextInt();
+		double c=Math.pow(a, b);
+		System.out.println(c);
+	}
+
+}
