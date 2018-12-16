9bb4efb8f9ca791c32e51183d59d170fc6dc7818
GOKULRAJ <41099493+GOKULGAYU@users.noreply.github.com>
2018-12-16 08:43:42 +0530

    Add files via upload

--- newo.java
+++ newo.java
@@ -1,20 +1,21 @@
 package gokulukog;
 import java.util.Scanner;
 public class newo {
-	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		int a=scan.nextInt();
-		int b=a,n,sum=0;
-		while (a>0) {
-			n=a%10;
-			sum=sum+(n*n*n);
-			a=a/10;
-		}
-			if (b==sum) {
-			System.out.println("Yes ,"+ b +"is Armstrong number");
-		}else {
-			System.out.println("No ,"+ b +"is Not an Armstrong number");
-		}
-	}
-
+	 public static void main(String[]args ) {
+		  Scanner scan=new Scanner(System.in);
+		  int a=scan.nextInt();
+		  int b[]=new int[a];
+		  int c[]=new int [a/2];
+		  for (int i=0;i<a;i++) {
+			  b[i]=scan.nextInt();
+		  }
+		  for(int k=0,i=0;i<a;i++) {
+			  for (int j=i+1;j<a;j++) {
+				  if(b[i]==b[j]) {
+					System.out.print(b[j]+" ");  
+				  }
+			  }
+		  }
+		  
+	 }
 }
