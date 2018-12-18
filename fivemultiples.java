07e26802b7fac21da08f3e2e719e976d828a372b
GOKULRAJ <41099493+GOKULGAYU@users.noreply.github.com>
2018-12-18 20:32:46 +0530

    Add files via upload

---
+++ fivemultiples.java
@@ -0,0 +1,13 @@
+package gokulukog;
+import java.util.Scanner;
+public class fivemultiples {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+       int a=scan.nextInt();
+       for(int i=1;i<=5;i++) {
+    	   System.out.print(a*i+" ");
+       }
+	}
+
+}
